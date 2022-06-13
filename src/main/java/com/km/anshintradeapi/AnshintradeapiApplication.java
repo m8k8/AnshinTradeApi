57380bd534eaf2b9929ea57135817b6f398d4939
向山奎 <mukaiyamakei@mukaiyamakeinoMacBook-Pro.local>
2022-06-13 19:49:10 +0900

    first commit

---
+++ src/main/java/com/km/anshintradeapi/AnshintradeapiApplication.java
@@ -0,0 +1,13 @@
+package com.km.anshintradeapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnshintradeapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnshintradeapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/km/anshintradeapi/AnshintradeapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.km.anshintradeapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnshintradeapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
